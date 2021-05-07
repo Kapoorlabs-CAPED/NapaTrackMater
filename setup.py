4dcfbcf64f07e15171aa7658b4f7d58a1092764c
kapoorlab <randomaccessiblekapoor@gmail.com>
2021-05-07 16:54:07 +0200

    new release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as f:
 
 setup(
     name="napatrackmater",
-    version='2.2.2',
+    version='2.2.3',
     author='Varun Kapoor,Claudia Carabana Garcia',
     author_email='randomaccessiblekapoor@gmail.com',
     url='https://github.com/kapoorlab/NapaTrackMater/',
