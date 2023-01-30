aafc70ab0d3cafaf4e5226c0881a3a054d4dbdfc
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-01-30 15:15:38 +0100

    test spot

--- napatrackmater/Trackmate.py
+++ napatrackmater/Trackmate.py
@@ -866,7 +866,7 @@ def get_spot_dataset(spot_dataset, spot_dataset_index, track_analysis_spot_keys,
         AllValues[posiz] = LocationZ
         AllValues[frame] = LocationT
 
-        for k in track_analysis_spot_keys.values():
+        for k in track_analysis_spot_keys.items():
 
             if (
                 k != track_id
