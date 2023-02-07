c63bdbd7da1d3d5fd345b89f5b29c877069c5aee
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-02-07 13:55:08 +0100

    veto radius

--- napatrackmater/Trackmate.py
+++ napatrackmater/Trackmate.py
@@ -152,10 +152,11 @@ class TrackMate(object):
                 volume = [prop.area for prop in properties]
                 intensity_mean = [prop.intensity_mean for prop in properties]
                 intensity_total = [prop.intensity_mean * prop.area for prop in properties]
+                bounding_boxes = [prop.bbox for prop in properties]
 
                 tree = spatial.cKDTree(centroids)
 
-                self._timed_channel_seg_image[str(i)] =  tree, centroids, labels, volume, intensity_mean, intensity_total
+                self._timed_channel_seg_image[str(i)] =  tree, centroids, labels, volume, intensity_mean, intensity_total, bounding_boxes
           
 
     def _get_attributes(self):
@@ -440,29 +441,39 @@ class TrackMate(object):
                         }
             
                         if self.channel_seg_image is not None:
-                                    tree, centroids, labels, volume, intensity_mean, intensity_total = self._timed_channel_seg_image[str(int(float(frame)))]
+                                    tree, centroids, labels, volume, intensity_mean, intensity_total, bounding_boxes = self._timed_channel_seg_image[str(int(float(frame)))]
                                     dist, index = tree.query(testlocation)
-                                    location = (int(centroids[index][0]), int(centroids[index][1]), int(centroids[index][2]))
-                                    QUALITY = volume[index]
-                                    RADIUS = math.pow(QUALITY, 1.0/3.0) * self.xcalibration * self.ycalibration * self.zcalibration
-                                    distance_cell_mask = self._get_boundary_dist(frame, location, RADIUS)
-                                    self.channel_unique_spot_properties[cell_id] = {
-                                            self.cellid_key: int(cell_id), 
-                                            self.frameid_key : int(float(Spotobject.get(self.frameid_key))),
-                                            self.zposid_key : round(float(centroids[index][0]), 3),
-                                            self.yposid_key : round(float(centroids[index][1]), 3),
-                                            self.xposid_key : round(float(centroids[index][2]), 3),
 
-                                            self.total_intensity_ch1_key : round(float(intensity_total[index])),
-                                            self.mean_intensity_ch1_key : round(float(intensity_mean[index])),
-                                            self.total_intensity_ch2_key : round(float(intensity_total[index])),
-                                            self.mean_intensity_ch2_key : round(float(intensity_mean[index])),
 
-                                            self.radius_key : round(float(RADIUS)),
-                                            self.quality_key : round(float(QUALITY)),
-                                            self.distance_cell_mask_key: round(float(distance_cell_mask),2)
+                                    bbox = bounding_boxes[index]
+                                    sizez = abs(bbox[0] - bbox[3])
+                                    sizey = abs(bbox[1] - bbox[4])
+                                    sizex = abs(bbox[2] - bbox[5]) 
+                                    veto_volume = sizex * sizey * sizez
+                                    veto_radius = math.pow(3 * veto_volume / (4 * math.pi), 1.0 / 3.0)
+    
+                                    if dist < veto_radius:
+                                            location = (int(centroids[index][0]), int(centroids[index][1]), int(centroids[index][2]))
+                                            QUALITY = volume[index]
+                                            RADIUS = math.pow(QUALITY, 1.0/3.0) * self.xcalibration * self.ycalibration * self.zcalibration
+                                            distance_cell_mask = self._get_boundary_dist(frame, location, RADIUS)
+                                            self.channel_unique_spot_properties[cell_id] = {
+                                                    self.cellid_key: int(cell_id), 
+                                                    self.frameid_key : int(float(Spotobject.get(self.frameid_key))),
+                                                    self.zposid_key : round(float(centroids[index][0]), 3),
+                                                    self.yposid_key : round(float(centroids[index][1]), 3),
+                                                    self.xposid_key : round(float(centroids[index][2]), 3),
 
-                                    } 
+                                                    self.total_intensity_ch1_key : round(float(intensity_total[index])),
+                                                    self.mean_intensity_ch1_key : round(float(intensity_mean[index])),
+                                                    self.total_intensity_ch2_key : round(float(intensity_total[index])),
+                                                    self.mean_intensity_ch2_key : round(float(intensity_mean[index])),
+
+                                                    self.radius_key : round(float(RADIUS)),
+                                                    self.quality_key : round(float(QUALITY)),
+                                                    self.distance_cell_mask_key: round(float(distance_cell_mask),2)
+
+                                            } 
 
                 if self.channel_seg_image is not None:                    
                         for Spotobject in self.xml_root.iter('Spot'):
