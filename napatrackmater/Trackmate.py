26e65e9543f8fe2234e6ce94bef7230de22ce3c9
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-02-09 16:39:53 +0100

    int casting

--- napatrackmater/Trackmate.py
+++ napatrackmater/Trackmate.py
@@ -1124,7 +1124,7 @@ def get_spot_dataset(spot_dataset, track_analysis_spot_keys, xcalibration, ycali
                            value = track_analysis_spot_keys["total_intensity"]
                            AllValues[value] = spot_dataset[v].astype("float")       
 
-                if v not in ignore_value for ignore_value in ignore_values:
+                if v not in any(ignore_value for ignore_value in ignore_values):
                     AllValues[v] = spot_dataset[v].astype("float")
 
         AllValues[posix] = round(LocationX,3)
