35d315c972e20ae03cebcfdb85e3b9309df017fe
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-02-15 19:09:26 +0100

    pytorch tricks

--- napatrackmater/clustering.py
+++ napatrackmater/clustering.py
@@ -139,9 +139,7 @@ def _model_output(model, clouds, labels, centroids):
        output_cluster_score = [max(torch.squeeze(cluster).detach().cpu().numpy()) for cluster in clusters]
        output_cluster_centroid = [tuple(torch.squeeze(centroid_input).detach().cpu().numpy()) for centroid_input in centroid_inputs]
        output_labels = [int(float(torch.squeeze(label_input).detach().cpu().numpy())) for label_input in label_inputs]
-       output_cluster_class = [np.argmax(score) for score in max_score]
-       print(output_labels, output_cluster_centroid)
-      
+       output_cluster_class = [np.argmax(torch.squeeze(cluster).detach().cpu().numpy()) for cluster in clusters]
 
        return output_labels, output_cluster_score, output_cluster_class, output_cluster_centroid              
 
