3d6989e24f9f542a849c765688f281bc6e5f35fc
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-02-25 15:44:05 +0100

    npm

--- napatrackmater/__init__.py
+++ napatrackmater/__init__.py
@@ -21,17 +21,17 @@ __all__ = (
 
 clear_models_and_aliases(DeepEmbeddedClustering, CloudAutoEncoder)
 
-register_model(CloudAutoEncoder, 'Nuclei_3D', 'https://zenodo.org/record/7677125/files/xenopus_nuclei_dgcnn_foldingnet_knn8.zip', 'fb476f050aacf81b61a3233ae69c6ad4' )
+register_model(CloudAutoEncoder, 'xenopus_nuclei_dgcnn_foldingnet_knn20', 'https://zenodo.org/record/7677125/files/xenopus_nuclei_dgcnn_foldingnet_knn8.zip', 'fb476f050aacf81b61a3233ae69c6ad4' )
 register_model(CloudAutoEncoder, 'Membrane_3D', '.zip', 'hash')
 
-register_model(DeepEmbeddedClustering, 'Nuclei_3D_cluster_3_class', 'https://zenodo.org/record/7677125/files/cluster_xenopus_nuclei_dgcnn_foldingnet_knn8_class3.zip', '26608aba6282788c2d7bf2e7bbf38711')
+register_model(DeepEmbeddedClustering, 'cluster_xenopus_nuclei_dgcnn_foldingnet_knn8_class3', 'https://zenodo.org/record/7677125/files/cluster_xenopus_nuclei_dgcnn_foldingnet_knn8_class3.zip', '26608aba6282788c2d7bf2e7bbf38711')
 register_model(DeepEmbeddedClustering, 'Membrane_3D_cluster', '.zip', 'hash')
 
 
-register_aliases(CloudAutoEncoder,  'xenopus_nuclei_dgcnn_foldingnet_knn20', 'Nuclei_3D',)
+register_aliases(CloudAutoEncoder, 'Nuclei_3D', 'xenopus_nuclei_dgcnn_foldingnet_knn20')
 register_aliases(CloudAutoEncoder, 'Membrane_3D', 'Membrane_3D')
 
-register_aliases(DeepEmbeddedClustering, 'cluster_xenopus_nuclei_dgcnn_foldingnet_knn8_class3', 'Nuclei_3D_cluster_3_class')
+register_aliases(DeepEmbeddedClustering, 'Nuclei_3D_cluster_3_class', 'cluster_xenopus_nuclei_dgcnn_foldingnet_knn8_class3')
 register_aliases(DeepEmbeddedClustering, 'Membrane_3D_cluster', 'Membrane_3D_cluster')
 
 del register_model, register_aliases, clear_models_and_aliases
