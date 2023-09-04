d12f8db50673287c3b504cf0703153bf1b476ac9
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-09-04 17:37:02 +0200

    update the packaging

---
+++ update_version.py
@@ -0,0 +1,22 @@
+import subprocess
+
+
+def update_version_file():
+    # Get the latest Git tag
+    tag = (
+        subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"])
+        .decode()
+        .strip()
+    )
+    if "v" in tag:
+        tag = tag.lstrip("v")
+    # Update _version.py
+    with open("src/napatrackmater/_version.py", "w") as version_file:
+        version_file.write(f'__version__ = version = "{tag}"\n')
+        version_file.write(
+            f'__version_tuple__ = version_tuple = {tuple(map(int, tag.split(".")))}\n'
+        )
+
+
+if __name__ == "__main__":
+    update_version_file()
