c9dffb6b82a83b40f8803cc8733786cf5a2355ab
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-10-07 19:55:13 +0200

    add sample data

--- src/napatrackmater/__init__.py
+++ src/napatrackmater/__init__.py
@@ -1,19 +1,47 @@
 from .pretrained import register_model, register_aliases, clear_models_and_aliases
 from .clustering import Clustering
 from .Trackmate import TrackMate, get_feature_dict
-from .Trackvector import TrackVector, create_analysis_vectors_dict, convert_tracks_to_arrays,perform_cosine_similarity, perform_pca
+from .Trackvector import (
+    TrackVector,
+    create_analysis_vectors_dict,
+    convert_tracks_to_arrays,
+    perform_cosine_similarity,
+    perform_pca,
+)
 from .DeepEmbeddedClustering import DeepEmbeddedClustering
 from .CloudAutoEncoder import CloudAutoEncoder
 import json
 from csbdeep.utils.tf import keras_import
+from tifffile import imread
+import os
+
 get_file = keras_import("utils", "get_file")
 
 
+def abspath(path):
+
+    base_path = os.path.abspath(os.path.dirname(__file__))
+    return os.path.join(base_path, path)
+
+
 def test_tracks_xenopus():
-    url = 'https://zenodo.org/record/8417322/files/example_tracks_napari_trackmate.zip'
-    hash = '589c7b1834b995adb2ae183604961f48'
-    track_directory = get_file(fname='example_tracks_napari_trackmate',origin=url,file_hash=hash)
-    return track_directory
+    url = "https://zenodo.org/record/8417322/files/example_tracks_napari_trackmate.zip"
+    hash = "589c7b1834b995adb2ae183604961f48"
+    current_directory = os.getcwd()
+    parent_directory = os.path.dirname(current_directory)
+    abspath(
+        get_file(
+            fname="example_tracks_napari_trackmate.zip",
+            origin=url,
+            file_hash=hash,
+            extract=True,
+            archive_format="zip",
+            cache_subdir=parent_directory,
+        )
+    )
+    extracted_folder = os.path.join(parent_directory, "nuclei_membrane_tracking")
+    image = imread(os.path.join(extracted_folder, "nuclei_timelapse_test_dataset.tif"))
+    return image
 
 
 def load_json(fpath):
@@ -33,7 +61,6 @@ __all__ = (
     "perform_pca",
     "DeepEmbeddedClustering",
     "load_json",
-
 )
 
 clear_models_and_aliases(DeepEmbeddedClustering, CloudAutoEncoder)
