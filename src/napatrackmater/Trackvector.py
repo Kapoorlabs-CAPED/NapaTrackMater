1d31d8d3ef8b50fdf4152e2a177bfc25cbabe34d
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-10-29 14:06:30 +0100

    cluster labels

--- src/napatrackmater/Trackvector.py
+++ src/napatrackmater/Trackvector.py
@@ -573,9 +573,9 @@ def unsupervised_clustering(
             == dynamic_track_array.shape[0]
         ), "Shape dynamic, shape and dynamic track arrays must have the same length"
         if shape_dynamic_track_array.shape[0] > 1:
-            shape_dynamic_covariance_matrix.append(compute_covariance_matrix(shape_dynamic_track_array)[:,0].astype(np.float32))
-            shape_covariance_matrix.append(compute_covariance_matrix(shape_track_array)[:,0].astype(np.float32))
-            dynamic_covariance_matrix.append(compute_covariance_matrix(dynamic_track_array)[:,0].astype(np.float32))
+            shape_dynamic_covariance_matrix.append(compute_covariance_matrix(shape_dynamic_track_array)[:,0].flatten().astype(np.float32))
+            shape_covariance_matrix.append(compute_covariance_matrix(shape_track_array)[:,0].flatten().astype(np.float32))
+            dynamic_covariance_matrix.append(compute_covariance_matrix(dynamic_track_array)[:,0].flatten().astype(np.float32))
             analysis_track_ids.append(track_id)
     
     shape_dynamic_covariance_matrices = np.dstack(shape_dynamic_covariance_matrix)
