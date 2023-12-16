c93fa23a8ca4a3ad882b609aa37f3bb625623e24
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-12-16 22:29:18 +0100

    remove pooling

--- src/napatrackmater/Trackvector.py
+++ src/napatrackmater/Trackvector.py
@@ -1606,22 +1606,13 @@ class MitosisNet(nn.Module):
         num_classes_class2,
     ):
         super().__init__()
-        self.densenet = DenseNet1d(
-            growth_rate=growth_rate,
-            block_config=block_config,
+        self.densenet = SimpleDenseNet1d(
+            features=features,
             num_init_features=num_init_features,
             in_channels=1,
             num_classes_1=num_classes_class1,
             num_classes_2=num_classes_class2,
         )
-        
-        #SimpleDenseNet1d(
-        #    features=features,
-        #    num_init_features=num_init_features,
-        #    in_channels=1,
-        #    num_classes_1=num_classes_class1,
-        #    num_classes_2=num_classes_class2,
-        #)
         
         #DenseNet1d(
         #    growth_rate=growth_rate,
