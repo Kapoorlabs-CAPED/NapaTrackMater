dea0e8600601d6445524d4cdab8c0e703eb9341a
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-12-16 03:49:17 +0100

    add a densenet 2d

--- src/napatrackmater/Trackvector.py
+++ src/napatrackmater/Trackvector.py
@@ -1363,6 +1363,154 @@ def compute_covariance_matrix(track_arrays, shape_array=5, feature_array=None):
     eigenvectors = eigenvectors[:, eigenvalue_order]
 
     return covariance_matrix, eigenvectors
+import torch.nn as nn
+
+class DenseLayer2D(nn.Module):
+    def __init__(self, in_channels, growth_rate, bottleneck_size, kernel_size):
+        super().__init__()
+        self.use_bottleneck = bottleneck_size > 0
+        self.num_bottleneck_output_filters = growth_rate * bottleneck_size
+        if self.use_bottleneck:
+            self.bn2 = nn.GroupNorm(1, in_channels)
+            self.act2 = nn.ReLU(inplace=True)
+            self.conv2 = nn.Conv2d(
+                in_channels, self.num_bottleneck_output_filters, kernel_size=1, stride=1
+            )
+        self.bn1 = nn.GroupNorm(1, self.num_bottleneck_output_filters)
+        self.act1 = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(
+            self.num_bottleneck_output_filters,
+            growth_rate,
+            kernel_size=kernel_size,
+            stride=1,
+            padding=kernel_size // 2,
+        )
+
+    def forward(self, x):
+        if self.use_bottleneck:
+            x = self.bn2(x)
+            x = self.act2(x)
+            x = self.conv2(x)
+        x = self.bn1(x)
+        x = self.act1(x)
+        x = self.conv1(x)
+        return x
+
+
+class DenseBlock2D(nn.Module):
+    def __init__(
+        self, num_layers, in_channels, growth_rate, kernel_size, bottleneck_size
+    ):
+        super().__init__()
+        self.num_layers = num_layers
+        for i in range(self.num_layers):
+            self.add_module(
+                f"denselayer{i}",
+                DenseLayer2D(
+                    in_channels + i * growth_rate,
+                    growth_rate,
+                    bottleneck_size,
+                    kernel_size,
+                ),
+            )
+
+    def forward(self, x):
+        layer_outputs = [x]
+        for _, layer in self.named_children():
+            x = layer(x)
+            layer_outputs.append(x)
+            x = torch.cat(layer_outputs, dim=1)
+        return x
+
+
+class TransitionBlock2D(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.bn = nn.GroupNorm(1, in_channels)
+        self.act = nn.ReLU(inplace=True)
+        self.conv = nn.Conv2d(
+            in_channels, out_channels, kernel_size=1, stride=1
+        )
+        self.pool = nn.AvgPool2d(kernel_size=2, stride=2)
+
+    def forward(self, x):
+        x = self.bn(x)
+        x = self.act(x)
+        x = self.conv(x)
+        x = self.pool(x)
+        return x
+
+
+class DenseNet2D(nn.Module):
+    def __init__(
+        self,
+        growth_rate=4,
+        block_config=(3, 3),
+        num_init_features=32,
+        bottleneck_size=2,
+        kernel_size=3,
+        in_channels=3,
+        num_classes_1=1,
+        num_classes_2=1,
+    ):
+        super().__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels, num_init_features, kernel_size=3),
+            nn.GroupNorm(1, num_init_features),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        )
+
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            block = DenseBlock2D(
+                num_layers=num_layers,
+                in_channels=num_features,
+                growth_rate=growth_rate,
+                kernel_size=kernel_size,
+                bottleneck_size=bottleneck_size,
+            )
+            self.features.add_module(f"denseblock{i}", block)
+            num_features = num_features + num_layers * growth_rate
+            if i != len(block_config) - 1:
+                trans = TransitionBlock2D(
+                    in_channels=num_features, out_channels=num_features // 2
+                )
+                self.features.add_module(f"transition{i}", trans)
+                num_features = num_features // 2
+
+        self.final_bn = nn.GroupNorm(1, num_features)
+        self.final_act = nn.ReLU(inplace=True)
+        self.final_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.classifier_1 = nn.Linear(num_features, num_classes_1)
+        self.classifier_2 = nn.Linear(num_features, num_classes_2)
+
+        for module in self.modules():
+            if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
+                nn.init.xavier_uniform_(module.weight)
+                if module.bias is not None:
+                    nn.init.constant_(module.bias, 0)
+
+    def forward_features(self, x):
+        out = self.features(x)
+        out = self.final_bn(out)
+        out = self.final_act(out)
+        out = self.final_pool(out)
+        return out
+
+    def forward(self, x):
+        features = self.forward_features(x)
+        features = features.squeeze(-1).squeeze(-1)
+        out_1 = self.classifier_1(features)
+        out_2 = self.classifier_2(features)
+        return out_1, out_2
+
+    def reset_classifier(self):
+        self.classifier = nn.Identity()
+
+    def get_classifier(self):
+        return self.classifier
 
 
 class DenseLayer(nn.Module):
