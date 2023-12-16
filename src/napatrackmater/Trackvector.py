75d4958c9788c18fe469bad3c97d814fa99b07db
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-12-16 02:30:26 +0100

    kernel 3

--- src/napatrackmater/Trackvector.py
+++ src/napatrackmater/Trackvector.py
@@ -1653,7 +1653,7 @@ def train_mitosis_neural_net(
 
     criterion_class1 = nn.CrossEntropyLoss()
     criterion_class2 = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
 
     if use_scheduler:
         milestones = [int(epochs * 0.5), int(epochs * 0.75)]
