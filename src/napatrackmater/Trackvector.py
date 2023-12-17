c32202cc8096e3a7fe37e751bb6bc99277c5f11d
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-12-17 22:10:42 +0100

    create mitosis data special

--- src/napatrackmater/Trackvector.py
+++ src/napatrackmater/Trackvector.py
@@ -1030,7 +1030,6 @@ def create_mitosis_training_data(
     dynamic_track_arrays = z_score_normalization(dynamic_track_arrays)
     count_label_dividing_0 = 0 
     count_label_dividing_1 = 0 
-    print(analysis_track_ids.shape[0])
     for idx in range(analysis_track_ids.shape[0]):
         current_track_id = analysis_track_ids[idx]
         filtered_data = global_shape_dynamic_dataframe[
@@ -1067,10 +1066,10 @@ def create_mitosis_training_data(
         features_shape_dynamic = shape_dynamic_track_arrays[idx, :].tolist()
         features_shape = shape_track_arrays[idx, :].tolist()
         features_dynamic = dynamic_track_arrays[idx, :].tolist()
-        if gt_label == 0 and samples_collected_0 >= 3 * min_samples_label_dividing:
-            continue
-        elif gt_label == 1 and samples_collected_1 >= min_samples_label_dividing:
-            continue
+        #if gt_label == 0 and samples_collected_0 >= 3 * min_samples_label_dividing:
+        #    continue
+        #elif gt_label == 1 and samples_collected_1 >= min_samples_label_dividing:
+        #    continue
         
 
         if gt_label == 0:
