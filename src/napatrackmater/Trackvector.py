51e15404dc95324c11ab71fdcfb8f5931a5491db
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-12-17 14:28:01 +0100

    remove pooling

--- src/napatrackmater/Trackvector.py
+++ src/napatrackmater/Trackvector.py
@@ -2065,7 +2065,7 @@ def predict_with_model(saved_model_path, saved_model_json, features_array, thres
     predicted_classes2 = []
     for idx in range(len(features_array)):
         feature_type = features_array[idx,:].tolist()
-        feature_type = z_score_normalization(feature_type.reshape(-1, 1))
+        feature_type = z_score_normalization(feature_type)
         feature_type = feature_type.flatten()
         features_tensor = torch.tensor(feature_type, dtype=torch.float32).to(device)
         new_data_with_channel = features_tensor.unsqueeze(1)
