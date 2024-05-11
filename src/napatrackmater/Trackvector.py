3f26871b5c7f09c4b624c064c6ad56c0307ae715
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-05-11 21:26:05 +0000

    npm

--- src/napatrackmater/Trackvector.py
+++ src/napatrackmater/Trackvector.py
@@ -2896,7 +2896,7 @@ class MitosisDataset(Dataset):
     
     def __getitem__(self, idx):
         array = self.arrays[idx]
-        array = torch.tensor(array).permute(1, 0)
+        array = torch.tensor(array).permute(1, 0).float()
         label = self.labels[idx]
         return array, label
    
