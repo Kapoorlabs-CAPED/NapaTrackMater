0a8cca5558a29913aa693367d0cc2758721bf142
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-09-27 17:10:02 +0000

    det ch

--- src/napatrackmater/Trackvector.py
+++ src/napatrackmater/Trackvector.py
@@ -264,7 +264,10 @@ class TrackVector(TrackMate):
             "DetectorSettings"
         )
         self.basicsettings = self.xml_content.find("Settings").find("BasicSettings")
-        self.detectorchannel = int(float(self.detectorsettings.get("TARGET_CHANNEL")))
+        try:
+           self.detectorchannel = int(float(self.detectorsettings.get("TARGET_CHANNEL")))
+        except TypeError:
+            self.detectorchannel = 1
         self.tstart = int(float(self.basicsettings.get("tstart")))
         self.tend = int(float(self.basicsettings.get("tend")))
         self.xmin = int(float(self.basicsettings.get("xstart")))
